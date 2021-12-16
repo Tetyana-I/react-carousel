d6fc88dd5f9b482e2c7b808a11d5c0fcffdeb613
Tetyana <iltaev2010@gmail.com>
2021-12-16 15:25:33 -0600

    finished carousel exercise

--- src/Carousel.js
+++ src/Carousel.js
@@ -10,30 +10,35 @@ function Carousel(props) {
   const card = props.cardData[cardIdx];
   const total = props.cardData.length;
   const goForward = () => setCardIdx(cardIdx + 1);
+  const goBack = () => setCardIdx(cardIdx - 1);
 
   return (
     <div className="Carousel">
       <h1>{props.title}</h1>
       <div className="Carousel-main">
-        <i
+        { (cardIdx === 0) ? null :
+        (<i
           className="fas fa-chevron-circle-left fa-2x"
-          onClick={goForward}
+          onClick={goBack}
           data-testid="left-arrow"
-        />
+        />) }
         <Card
           caption={card.caption}
           src={card.src}
           currNum={cardIdx + 1}
           totalNum={total}
         />
-        <i
+        { (cardIdx === total-1) ? null :
+        (<i
           className="fas fa-chevron-circle-right fa-2x"
           onClick={goForward}
           data-testid="right-arrow"
-        />
+        />)          
+        }
       </div>
     </div>
   );
+
 }
 
 Carousel.defaultProps = {
--- src/Carousel.test.js
+++ src/Carousel.test.js
@@ -2,6 +2,19 @@ import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import Carousel from "./Carousel";
 
+// smoke test
+it("renders without crashing", function() {
+  render(<Carousel />);
+});
+
+
+// snapshot test
+it("matches snapshot", function() {
+  const {asFragment} = render(<Carousel />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
 it("works when you click on the right arrow", function() {
   const { queryByTestId, queryByAltText } = render(<Carousel />);
 
@@ -16,4 +29,57 @@ it("works when you click on the right arrow", function() {
   // expect the second image to show, but not the first
   expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
   expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+});
+
+
+// test that allows to catch a left arrow bug
+it("works when you click on the left arrow", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+
+  // move to the second image in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+
+  // check that we are on the second image
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).not.toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).toBeInTheDocument();
+
+  // move back in the carousel
+  const leftArrow = queryByTestId("left-arrow");
+  fireEvent.click(leftArrow);
+
+  // expect the first image to show, but not the second
+  expect(queryByAltText("Photo by Richard Pasquarella on Unsplash")).toBeInTheDocument();
+  expect(queryByAltText("Photo by Pratik Patel on Unsplash")).not.toBeInTheDocument();
+
+});
+
+
+// Tests to check that the left arrow is missing when you’re on the first image, 
+// and that the right arrow is missing when you’re on the last image in the carousel.
+
+it("right arrow is missing if you are on the last image", function() {
+  const { queryByTestId, queryByAltText } = render(<Carousel />);
+  
+  // move to the third image in the carousel
+  const rightArrow = queryByTestId("right-arrow");
+  fireEvent.click(rightArrow);
+  fireEvent.click(rightArrow);
+  
+  // check that we are on the third image in the carousel
+  expect(queryByAltText("Photo by Josh Post on Unsplash")).toBeInTheDocument();
+
+  // check that right arrow is missing
+  expect(rightArrow).not.toBeInTheDocument();
+
+});
+
+
+it("left arrow is missing if you are on the first image", function() {
+  const { queryByTestId } = render(<Carousel />);
+  const leftArrow = queryByTestId("left-arrow");
+
+  // check that left arrow is missing
+  expect(leftArrow).not.toBeInTheDocument();
+
 });
